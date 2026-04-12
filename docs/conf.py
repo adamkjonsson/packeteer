33d0b51fcb02705350520a1eda073fc3b81da8d4
Adam Jonsson <adam.jonsson@live.se>
2026-04-12 16:41:28 +0200

    Updated to version 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 
 project = "packeteer"
 author = "Adam Jonsson"
-release = "0.1.0"
+release = "0.2.0"
 copyright = f"2026, {author}"
 
 # ── Extensions ────────────────────────────────────────────────────────────────
